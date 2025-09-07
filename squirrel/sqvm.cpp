d8f14a2aced83ca3c8dff6ccf0941ae696b2b79b
zonde306 <anonymous@localhost>
2025-09-07 18:19:21 +0800

    feat: empty strings, tables, and arrays evaluate to false.

--- squirrel/sqvm.cpp
+++ squirrel/sqvm.cpp
@@ -682,6 +682,13 @@ bool SQVM::IsEqual(const SQObjectPtr &o1,const SQObjectPtr &o2,bool &res)
 
 bool SQVM::IsFalse(SQObjectPtr &o)
 {
+    switch(sq_type(o))
+    {
+        case OT_STRING:		return _string(o)->_len <= 0;
+        case OT_TABLE:		return _table(o)->CountUsed() <= 0;
+        case OT_ARRAY:		return _array(o)->Size() <= 0;
+        default:;
+    }
     if(((sq_type(o) & SQOBJECT_CANBEFALSE)
         && ( ((sq_type(o) == OT_FLOAT) && (_float(o) == SQFloat(0.0))) ))
 #if !defined(SQUSEDOUBLE) || (defined(SQUSEDOUBLE) && defined(_SQ64))
